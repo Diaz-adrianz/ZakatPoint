77c9fbd596800fdc13863ea9a1bf752204cbea97
Diaz-Adrianz <diazadrianz191205@gmail.com>
2025-07-12 12:18:15 +0700

    style: run formatter

--- resources/js/pages/new-village.tsx
+++ resources/js/pages/new-village.tsx
@@ -8,30 +8,30 @@ import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { District, Province, Regency, Village } from '@/types/region';
 import { Head, useForm } from '@inertiajs/react';
-import { FormEventHandler, useEffect, useState } from 'react';
+import { FormEventHandler, useState } from 'react';
 
 type VillageForm = {
-    province: string
-    city: string
-    district: string
-    village: string
-    postal_code: string,
-    longitude: string,
-    latitude: string
-    email_village: string
-}
+    province: string;
+    city: string;
+    district: string;
+    village: string;
+    postal_code: string;
+    longitude: string;
+    latitude: string;
+    email_village: string;
+};
 
-export default function NewVillage({provinces}: {provinces: Province[] }) {
-     const { data, setData, post, errors, processing } = useForm<Required<VillageForm>>({
+export default function NewVillage({ provinces }: { provinces: Province[] }) {
+    const { data, setData, post, errors, processing } = useForm<Required<VillageForm>>({
         province: '',
-        city: ""
-        ,district: ''
-        ,village:""
-        ,postal_code: ''
-        ,longitude: ''
-        ,latitude: ''
-        ,email_village: ''
-     });
+        city: '',
+        district: '',
+        village: '',
+        postal_code: '',
+        longitude: '',
+        latitude: '',
+        email_village: '',
+    });
 
     const submit: FormEventHandler = (e) => {
         e.preventDefault();
@@ -41,50 +41,41 @@ export default function NewVillage({provinces}: {provinces: Province[] }) {
         });
     };
 
-    const [regencies, setRegencies] = useState<Regency[]>([])
+    const [regencies, setRegencies] = useState<Regency[]>([]);
     const getRegencies = async (provinceName: string) => {
-        setData("city", '')
-        setData('district', '')
-        setData('village', '')
+        setData('city', '');
+        setData('district', '');
+        setData('village', '');
 
-        const prov = provinces.find(p => p.name == provinceName)
+        const prov = provinces.find((p) => p.name == provinceName);
         if (prov)
-            fetch("/api/regencies/" + prov.id)
-                .then(res => res.json())
-                .then(data =>  
-                    setRegencies(data)
-                )
-    } 
+            fetch('/api/regencies/' + prov.id)
+                .then((res) => res.json())
+                .then((data) => setRegencies(data));
+    };
 
-
-    const [districts, setDistricts] = useState<District[]>([])
+    const [districts, setDistricts] = useState<District[]>([]);
     const getDistricts = async (regencyName: string) => {
-        setData('district', '')
-        setData('village', '')
+        setData('district', '');
+        setData('village', '');
 
-        
-        const reg = regencies.find(p => p.name == regencyName)
+        const reg = regencies.find((p) => p.name == regencyName);
         if (reg)
-            fetch("/api/districts/" + reg.id)
-                .then(res => res.json())
-                .then(data =>  
-                    setDistricts(data)
-                )
-    } 
+            fetch('/api/districts/' + reg.id)
+                .then((res) => res.json())
+                .then((data) => setDistricts(data));
+    };
 
-    const [villages, setVillages] = useState<Village[]>([])
+    const [villages, setVillages] = useState<Village[]>([]);
     const getVillages = async (districtName: string) => {
-        setData('village', '')
-        
-        const dis = districts.find(p => p.name == districtName)
-        if (dis)
-            fetch("/api/villages/" + dis.id)
-                .then(res => res.json())
-                .then(data =>  
-                    setVillages(data)
-                )
-    } 
+        setData('village', '');
 
+        const dis = districts.find((p) => p.name == districtName);
+        if (dis)
+            fetch('/api/villages/' + dis.id)
+                .then((res) => res.json())
+                .then((data) => setVillages(data));
+    };
 
     return (
         <>
@@ -93,143 +84,141 @@ export default function NewVillage({provinces}: {provinces: Province[] }) {
                 <h3 className="typo-h3">Daftarkan Desa</h3>
 
                 <form onSubmit={submit}>
+                    <Card>
+                        <CardContent className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label>Provinsi</Label>
+                                <Select
+                                    value={data.province}
+                                    onValueChange={(v) => {
+                                        setData('province', v);
+                                        getRegencies(v);
+                                    }}
+                                >
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Pilih provinsi" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {provinces.map((p, i) => (
+                                            <SelectItem key={i} value={p.name}>
+                                                {p.name}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <InputError className="mt-2" message={errors.province} />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label>Kota/Kabupaten</Label>
+                                <Select
+                                    disabled={regencies.length == 0}
+                                    value={data.city}
+                                    onValueChange={(v) => {
+                                        setData('city', v);
+                                        getDistricts(v);
+                                    }}
+                                >
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Pilih kota/kabupaten" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {regencies.map((p, i) => (
+                                            <SelectItem key={i} value={p.name}>
+                                                {p.name}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <InputError className="mt-2" message={errors.city} />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label>Kecamatan</Label>
+                                <Select
+                                    disabled={districts.length == 0}
+                                    value={data.district}
+                                    onValueChange={(v) => {
+                                        setData('district', v);
+                                        getVillages(v);
+                                    }}
+                                >
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Pilih kecamatan" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {districts.map((p, i) => (
+                                            <SelectItem key={i} value={p.name}>
+                                                {p.name}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <InputError className="mt-2" message={errors.district} />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label>Desa</Label>
+                                <Select
+                                    disabled={villages.length == 0}
+                                    value={data.village}
+                                    onValueChange={(v) => {
+                                        setData('village', v);
+                                    }}
+                                >
+                                    <SelectTrigger>
+                                        <SelectValue placeholder="Pilih desa" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {villages.map((p, i) => (
+                                            <SelectItem key={i} value={p.name}>
+                                                {p.name}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                                <InputError className="mt-2" message={errors.village} />
+                            </div>
+                            <div className="grid gap-2">
+                                <Label>Kode pos</Label>
 
-                <Card>
-                    <CardContent className='flex flex-col gap-6'>
-                        <div className="grid gap-2">
-                            <Label>Provinsi</Label>
-                            <Select 
-                                value={data.province} 
-                                onValueChange={v => {
-                                    setData("province", v);
-                                    getRegencies(v)
-                                }}
-                            >
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Pilih provinsi" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {
-                                        provinces.map((p, i) => (
-                                            <SelectItem key={i} value={p.name}>{p.name}</SelectItem>
-                                        ))
-                                    }
-                                </SelectContent>
-                            </Select>
-                            <InputError className="mt-2" message={errors.province} />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label>Kota/Kabupaten</Label>
-                            <Select 
-                                disabled={regencies.length == 0}
-                                value={data.city} 
-                                onValueChange={v => {
-                                    setData("city", v);
-                                    getDistricts(v)
-                                }}
-                            >
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Pilih kota/kabupaten" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {
-                                        regencies.map((p, i) => (
-                                            <SelectItem key={i} value={p.name}>{p.name}</SelectItem>
-                                        ))
-                                    }
-                                </SelectContent>
-                            </Select>
-                            <InputError className="mt-2" message={errors.city} />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label>Kecamatan</Label>
-                            <Select 
-                                disabled={districts.length == 0}
-                                value={data.district} 
-                                onValueChange={v => {
-                                    setData("district", v);
-                                    getVillages(v)
-                                }}
-                            >
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Pilih kecamatan" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {
-                                        districts.map((p, i) => (
-                                            <SelectItem key={i} value={p.name}>{p.name}</SelectItem>
-                                        ))
-                                    }
-                                </SelectContent>
-                            </Select>
-                            <InputError className="mt-2" message={errors.district} />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label>Desa</Label>
-                            <Select 
-                                disabled={villages.length == 0}
-                                value={data.village} 
-                                onValueChange={v => {
-                                    setData("village", v);
-                                }}
-                            >
-                                <SelectTrigger>
-                                    <SelectValue placeholder="Pilih desa" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    {
-                                        villages.map((p, i) => (
-                                            <SelectItem key={i} value={p.name}>{p.name}</SelectItem>
-                                        ))
-                                    }
-                                </SelectContent>
-                            </Select>
-                            <InputError className="mt-2" message={errors.village} />
-                        </div>
-                        <div className="grid gap-2">
-                            <Label>Kode pos</Label>
+                                <Input
+                                    value={data.postal_code}
+                                    onChange={(e) => setData('postal_code', e.target.value)}
+                                    required
+                                    autoComplete="postal_code"
+                                    placeholder="Kode pos"
+                                />
 
-                            <Input
-                                value={data.postal_code}
-                                onChange={(e) => setData('postal_code', e.target.value)}
-                                required
-                                autoComplete="postal_code"
-                                placeholder="Kode pos"
-                            />
+                                <InputError className="mt-2" message={errors.postal_code} />
+                            </div>
 
-                            <InputError className="mt-2" message={errors.postal_code} />
-                        </div>
+                            <div className="h-80 w-full">
+                                <MapPicker
+                                    latitude={+data.latitude}
+                                    longitude={+data.longitude}
+                                    onChange={({ latitude, longitude }) => {
+                                        setData('longitude', longitude.toString());
+                                        setData('latitude', latitude.toString());
+                                    }}
+                                />
+                            </div>
 
-                        <div className="w-full h-80">
-                            <MapPicker
-                                latitude={+data.latitude}
-                                longitude={+data.longitude}
-                                onChange={({latitude, longitude}) => {
-                                    setData("longitude", longitude.toString())
-                                    setData("latitude", latitude.toString())
+                            <div className="grid gap-2">
+                                <Label>Email desa</Label>
 
-                                }}    
-                            />
-                        </div>
+                                <Input
+                                    type="email"
+                                    value={data.email_village}
+                                    onChange={(e) => setData('email_village', e.target.value)}
+                                    required
+                                    autoComplete="email_village"
+                                    placeholder="Email desa"
+                                />
 
-                        <div className="grid gap-2">
-                            <Label>Email desa</Label>
+                                <InputError className="mt-2" message={errors.email_village} />
+                            </div>
 
-                            <Input
-                                type='email'
-                                value={data.email_village}
-                                onChange={(e) => setData('email_village', e.target.value)}
-                                required
-                                autoComplete="email_village"
-                                placeholder="Email desa"
-                            />
-
-                            <InputError className="mt-2" message={errors.email_village} />
-                        </div>
-
-                        <Button disabled={processing}>Daftar</Button>
-                    </CardContent>
-                </Card>
+                            <Button disabled={processing}>Daftar</Button>
+                        </CardContent>
+                    </Card>
                 </form>
             </Container>
         </>
--- resources/js/types/region.d.ts
+++ resources/js/types/region.d.ts
@@ -1,22 +1,22 @@
 export interface Province {
-    id: string,
-    name: string
+    id: string;
+    name: string;
 }
 
 export interface Regency {
-    id: string,
-    province_id: string
-    name: string
+    id: string;
+    province_id: string;
+    name: string;
 }
 
 export interface District {
-    id: string,
-    regency_id: string
-    name: string
+    id: string;
+    regency_id: string;
+    name: string;
 }
 
 export interface Village {
-    id: string,
-    district_id: string
-    name: string
+    id: string;
+    district_id: string;
+    name: string;
 }
