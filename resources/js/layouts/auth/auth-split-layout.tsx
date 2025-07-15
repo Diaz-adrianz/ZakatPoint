381e3b8dd3c310533340b5e8723cf18ab32f958d
Diaz-Adrianz <diazadrianz191205@gmail.com>
2025-07-15 20:00:13 +0700

    style: login image

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -14,7 +14,8 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-                <div className="absolute inset-0 bg-zinc-900" />
+                <img src='https://www.megasyariah.co.id/bms-new/edukasi-tips/mustahik_adalah.png' className="absolute top-0 size-full object-cover left-0"/>
+                <div className="absolute inset-0 bg-zinc-800/60 size-full z-1" />
                 <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
                     {/* <AppLogoIcon className="mr-2 size-8 fill-current text-white" /> */}
                     {name}
